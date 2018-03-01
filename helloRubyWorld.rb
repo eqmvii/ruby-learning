34b3df6fb78391daa3abfab7c49146aa8f553313
eqmvii <coelomate@gmail.com>
2018-03-01 16:34:08 -0500

    expanded hello world

--- helloRubyWorld.rb
+++ helloRubyWorld.rb
@@ -9,3 +9,34 @@ print "\n"
 1.upto(9){|x| print x }
 
 print "\n"
+
+name = "Eric"
+
+print "Hello #{name}! This was interpolated."
+
+print "\n"
+
+puts "Test sort vs. sort! - the former returns a new copy, the latter modifies in place"
+
+myArray = [4,8,2,4,1,10,5]
+
+p myArray
+
+puts
+
+p myArray.sort
+
+puts
+
+p myArray
+
+puts
+
+p myArray.sort!
+
+puts
+
+p myArray
+
+puts
+
